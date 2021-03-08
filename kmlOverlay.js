56e004aa768ebb591d80e05d751fb50d71121159
ACH1980 <m.hoffmann@kanjias.de>
2021-03-08 22:23:50 +0100

    Create kmlOverlay.js

---
+++ kmlOverlay.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         KML Overlay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://www.flightradar24.com/*
+// @icon         https://www.google.com/s2/favicons?domain=flightradar24.com
+// @run-at       document-end
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    function displayKml(){
+        if (!window.google || !window.map || !window.map.__gm) return window.setTimeout(displayKml, 1000);
+        var src = 'https://tmp.kanjias.dev/TFR.kml';
+
+        var ctaLayer = new google.maps.KmlLayer({
+            url: src,
+            map: window.map
+        });
+    }
+
+    function init() {
+        const e = document.getElementById("map_canvas");
+        if (!e) return window.setTimeout(init, 1000);
+        e.onload = displayKml();
+    }
+
+    init();
+})();
